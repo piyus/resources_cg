32fa9cb32cfe5bbde24660ecd41d123a9ec55a85
piyus kedia <piyuskedia@gmail.com>
2023-05-27 19:39:58 +0530

    added scripts

---
+++ runspec.sh
@@ -0,0 +1,3 @@
+source shrc
+runcpu --size=ref --action=clobber --copies=1 --tune=base --noreportable --iterations=1 505.mcf_r 502.gcc_r 500.perlbench_r 525.x264_r 557.xz_r 519.lbm_r 538.imagick_r 544.nab_r
+runcpu --size=ref --action=run --copies=1 --tune=base --noreportable --iterations=1 505.mcf_r 502.gcc_r 500.perlbench_r 525.x264_r 557.xz_r 519.lbm_r 538.imagick_r 544.nab_r
--- spec.sh
+++ spec.sh
@@ -1,3 +1,11 @@
-sudo mount -t iso9660 -o ro,exec,loop cpu2017-1_0_5.iso /mnt
+mkdir t
+mkdir spec
+sudo mount -t iso9660 -o ro,exec,loop cpu2017-1_0_5.iso ./t
+cd t
+sh install.sh -f -d ../spec
+sudo umount t
+cd ..
+rm -rf t
+cp ../resources/default.cfg ./spec/config
+cp ../resources/runspec.sh ./spec
 
-# go to /mnt
